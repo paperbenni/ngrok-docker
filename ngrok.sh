2df57de7f4024f062ec5b209f1a745ede024b04c
paperbenni <paperbenni@gmail.com>
2018-07-15 21:54:33 +0200

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-curl -o ngrok http://ngrok.surge.sh/ngrok
+gitexe() {
+        curl raw.githubusercontent.com/paperbenni/"$1"/master/"$2".sh | bash
+}
 
-curl "https://gist.githubusercontent.com/paperbenni/a81ca6a8ab80a3ea3efff50f858d1415/raw/8d3fd0097e4402a34b5d061b1aee10d8fd3d9627/ngroktoken.sh" | bash
+
+gitexe ngrok.sh ngroktoken
+
 TOKEN=$(cat ./token.txt)
 chmod +x ./ngrok
 ngrok authtoken "$TOKEN"
