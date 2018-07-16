406bda93a4203f59b0a045b3c55dca6239df375e
paperbenni <paperbenni@gmail.com>
2018-07-16 16:15:54 +0200

    Update ngroktoken.sh

--- ngroktoken.sh
+++ ngroktoken.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl "https://gist.githubusercontent.com/paperbenni/a81ca6a8ab80a3ea3efff50f858d1415/raw/4c4223f3520ed68ffc5a165b17e7cc03d92f3cca/ngroktoken.txt" > ./ngroktokens.txt
+curl "https://raw.githubusercontent.com/paperbenni/ngrok.sh/master/tokens.txt" > ./ngroktokens.txt
 shuf -n 1 ./ngroktokens.txt > token.txt
 rm ./ngroktokens.txt
 cat token.txt
